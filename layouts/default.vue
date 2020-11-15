0c60d791f302853b61b2b18aafee51883d85f5e3
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-11-15 22:41:35 +0200

    fix adaptive

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,9 @@
   <el-container>
     <el-main>
       <el-row type="flex" justify="center">
-        <el-col :span="4"><nuxt /> </el-col>
+        <el-col :span="24" :xs="24" :sm="10" :md="6" :lg="4" :xl="4"
+          ><nuxt />
+        </el-col>
       </el-row>
     </el-main>
   </el-container>
